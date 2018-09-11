5568029d6ca615b47eba4884b6d45b6fd8a40cb0
Stephen Courtier <steves0089@gmail.com>
2018-09-11 09:37:40 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  i = 0 
+  new_array = []
+  block_given?
+    while i < array.length
+      new_array.push(yield(array[i])) 
+    end
+    new_array
+  else 
+  "No block given"
+end
 
