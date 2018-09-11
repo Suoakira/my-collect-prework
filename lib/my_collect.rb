03ed5b82235295b62c5d6228ac65025e615feebe
Stephen Courtier <steves0089@gmail.com>
2018-09-11 09:39:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,10 @@
 def my_collect(array)
   i = 0 
   new_array = []
-  block_given?
     while i < array.length
-    
       new_array.push(yield(array[i]))
       i += 1
     end
     new_array
-  else
-    "puts no block given"
 end
 
